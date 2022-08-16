23124c8c17985de3f94f6577473c0cf263d047b5
skris92 <szurcsik92@gmail.com>
2022-08-16 18:37:43 +0200

    Initial data seeder

---
+++ SkrisForum.Data/DataSeeder.cs
@@ -0,0 +1,48 @@
+﻿using SkrisForum.Data.Entity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SkrisForum.Data
+{
+    public class DataSeeder
+    {
+        private readonly SkrisForumDBContext _dbContext;
+
+        public DataSeeder(SkrisForumDBContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public void Seed()
+        {
+            _dbContext.Database.EnsureCreated();
+
+            if (!_dbContext.Users.Any())
+            {
+                var seedUsers = new User[2]
+                {
+                    new User()
+                    {
+                        Email = "admin@admin.com",
+                        Username = "admin",
+                        HashedPassword = BCrypt.Net.BCrypt.HashPassword("1234"),
+                        Role = UserRole.ADMIN
+            },
+                    new User()
+                    {
+                        Email = "user@user.com",
+                        Username = "user",
+                        HashedPassword = BCrypt.Net.BCrypt.HashPassword("1234"),
+                        Role = UserRole.USER
+                    }
+                };
+
+                _dbContext.Users.AddRange(seedUsers);
+                _dbContext.SaveChanges();
+            }
+        }
+    }
+}
