2512dde748b028c5585200e72ea6d45ee3a0654d
skris92 <szurcsik92@gmail.com>
2022-08-16 14:57:47 +0200

    User entity

---
+++ SkrisForum.Data/Entity/User.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SkrisForum.Data.Entity
+{
+    public class User
+    {
+        public Guid Id { get; set; }
+
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+
+        [Required]
+        [MaxLength(20)]
+        public string Username { get; set; }
+
+        [Required]
+        public string HashedPassword { get; set; }
+
+        [Required]
+        public UserRole Role { get; set; }
+    }
+}
