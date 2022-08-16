1b8d64430e9119b0febdb6f92b264383057a63d3
skris92 <szurcsik92@gmail.com>
2022-08-16 15:00:26 +0200

    SkrisForumDBContext service registered

--- SkrisForum/Program.cs
+++ SkrisForum/Program.cs
@@ -1,3 +1,6 @@
+using Microsoft.EntityFrameworkCore;
+using SkrisForum.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +9,12 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+// Add database service
+builder.Services.AddDbContext<SkrisForumDBContext>(options =>
+{
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
+});
 
 var app = builder.Build();
 
