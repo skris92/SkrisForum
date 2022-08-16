c6d750a7cbb3319864be4f66908e6989f10a0750
skris92 <szurcsik92@gmail.com>
2022-08-16 19:27:13 +0200

    CRUD methods

---
+++ SkrisForum.Data/Repositories/IRepository.cs
@@ -0,0 +1,11 @@
+﻿namespace SkrisForum.Data.Repositories
+{
+    public interface IRepository<T>
+    {
+        Task Add(T entity);
+        Task Delete(Guid id);
+        Task<T> GetAll();
+        Task<T> GetById(Guid id);
+        Task Update(T entity);
+    }
+}
