b0406bf30cd9732ea29323d5792744799dfb85c2
skris92 <szurcsik92@gmail.com>
2022-08-17 13:49:30 +0200

    User extension methods

---
+++ SkrisForum.Core/Extensions/UserExtensions.cs
@@ -0,0 +1,18 @@
+﻿using SkrisForum.Core.Model.UserDTOs;
+using SkrisForum.Data.Entities;
+
+namespace SkrisForum.Core.Extensions
+{
+    public static class UserExtensions
+    {
+        public static UserViewDTO ToUserViewDTO(this User user)
+        {
+            return new UserViewDTO
+            {
+                Id = user.Id,
+                Email = user.Email,
+                Username = user.Username
+            };
+        }
+    }
+}
