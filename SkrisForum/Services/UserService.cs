b4cd160ff7da64ea40c41cd1b5aa45a815f22002
skris92 <szurcsik92@gmail.com>
2022-08-17 13:50:16 +0200

    User service

---
+++ SkrisForum/Services/UserService.cs
@@ -0,0 +1,22 @@
+﻿using SkrisForum.Core.Extensions;
+using SkrisForum.Core.Model.UserDTOs;
+using SkrisForum.Data.Entities;
+using SkrisForum.Data.Repositories;
+
+namespace SkrisForum.Services
+{
+    public class UserService
+    {
+        private readonly IRepository<User> _userRepository;
+
+        public UserService(IRepository<User> userRepository)
+        {
+            _userRepository = userRepository;
+        }
+
+        public async Task<List<UserViewDTO>> GetAllUsers()
+        {
+            return (await _userRepository.GetAll()).Select(user => user.ToUserViewDTO()).ToList();
+        }
+    }
+}
