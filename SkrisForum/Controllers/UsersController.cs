736737a5c9a76726c75b3a5434cb19adea40ae35
skris92 <szurcsik92@gmail.com>
2022-08-17 13:49:57 +0200

    Users controller

---
+++ SkrisForum/Controllers/UsersController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using SkrisForum.Core.Model.UserDTOs;
+using SkrisForum.Services;
+
+namespace SkrisForum.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsersController : ControllerBase
+    {
+        private readonly UserService _userService;
+
+        public UsersController(UserService userService)
+        {
+            _userService = userService;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<List<UserViewDTO>>> GetAllUsers()
+        {
+            return await _userService.GetAllUsers();
+        }
+    }
+}
