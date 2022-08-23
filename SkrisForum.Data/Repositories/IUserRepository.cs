1ee2fca7401c2f0d7723cbe682d56df85495793e
skris92 <szurcsik92@gmail.com>
2022-08-23 13:11:37 +0200

    User repository interface with method GetByUsername

---
+++ SkrisForum.Data/Repositories/IUserRepository.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SkrisForum.Data.Repositories
+{
+    public interface IUserRepository<User> : IRepository<User>
+    {
+        Task<User> GetByUsername(string username);
+    }
+}
