da496f6ea3f49d045c403321fe23b7581f72e643
skris92 <szurcsik92@gmail.com>
2022-08-23 13:09:35 +0200

    UserLoginDTO

---
+++ SkrisForum.Core/Model/UserDTOs/UserLoginDTO.cs
@@ -0,0 +1,12 @@
+﻿using SkrisForum.Data.Entities;
+
+namespace SkrisForum.Core.Model.UserDTOs
+{
+    public class UserLoginDTO
+    {
+        public Guid Id { get; set; }
+        public string Username { get; set; }
+        public string HashedPassword { get; set; }
+        public UserRole Role { get; set; }
+    }
+}
