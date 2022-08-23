e94a0d29d05b427813edb9788742f63830f5bd5e
skris92 <szurcsik92@gmail.com>
2022-08-23 15:07:30 +0200

    AddUser and GetUserById services

--- SkrisForum/Services/UserService.cs
+++ SkrisForum/Services/UserService.cs
@@ -19,11 +19,28 @@ namespace SkrisForum.Services
             return (await _userRepository.GetAll()).Select(user => user.ToUserViewDTO()).ToList();
         }
 
+        public async Task<UserViewDTO> GetUserById(Guid userId)
+        {
+            User user = await _userRepository.GetById(userId);
+            return user.ToUserViewDTO();
+        }
+
         public async Task<UserLoginDTO> GetLoginDTOByUsername(string username)
         {
             var user = await _userRepository.GetByUsername(username);
 
             return user.ToUserLoginDTO();
+        }
+
+        public async Task<UserViewDTO> AddUser(UserCreateDTO newUser)
+        {
+            newUser.Role = "USER";
+            newUser.Password = BCrypt.Net.BCrypt.HashPassword(newUser.Password);
+
+            var userEntity = newUser.ToUserEntity();
+            await _userRepository.Add(userEntity);
+
+            return userEntity.ToUserViewDTO();
         }
     }
 }
