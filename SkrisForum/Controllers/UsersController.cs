b48af95a741a13369a3930e9c24ff45d028abffb
skris92 <szurcsik92@gmail.com>
2022-08-23 15:06:50 +0200

    AddUser and GetUserById endpoints

--- SkrisForum/Controllers/UsersController.cs
+++ SkrisForum/Controllers/UsersController.cs
@@ -1,6 +1,8 @@
 ﻿using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using SkrisForum.Core.Model.AuthenticationModels.Responses;
 using SkrisForum.Core.Model.UserDTOs;
 using SkrisForum.Services;
 
@@ -22,6 +24,34 @@ namespace SkrisForum.Controllers
         public async Task<ActionResult<List<UserViewDTO>>> GetAllUsers()
         {
             return await _userService.GetAllUsers();
+        }
+
+        [Authorize(Roles = "ADMIN, USER")]
+        [HttpGet("{userId}", Name = "GetUserById")]
+        public async Task<ActionResult<UserViewDTO>> GetUserById(Guid userId)
+        {
+            try
+            {
+                return await _userService.GetUserById(userId);
+            }
+            catch (Exception)
+            {
+                return BadRequest(new ErrorResponse("Non-existing user"));
+            }
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<UserViewDTO>> AddUser(UserCreateDTO newUser)
+        {
+            try
+            {
+                var createdUser = await _userService.AddUser(newUser);
+                return CreatedAtRoute("GetUserById", new {userId = createdUser.Id}, createdUser);
+            }
+            catch (DbUpdateException e)
+            {
+                return BadRequest(new ErrorResponse(e.Message));
+            }
         }
     }
 }
