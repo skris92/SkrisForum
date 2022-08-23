ae896df6e7852d06964ea78e3efe5e667d9c2415
skris92 <szurcsik92@gmail.com>
2022-08-23 13:09:12 +0200

    ErrorResponse class

---
+++ SkrisForum.Core/Model/AuthenticationModels/Responses/ErrorResponse.cs
@@ -0,0 +1,17 @@
+﻿namespace SkrisForum.Core.Model.AuthenticationModels.Responses
+{
+    public class ErrorResponse
+    {
+        public IEnumerable<string> ErrorMessages { get; set; }
+
+        public ErrorResponse(IEnumerable<string> errorMessages)
+        {
+            ErrorMessages = errorMessages;
+        }
+
+        public ErrorResponse(string errorMessage) : this(new List<string>() { errorMessage })
+        {
+
+        }
+    }
+}
