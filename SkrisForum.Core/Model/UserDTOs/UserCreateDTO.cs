0a32b9dc8c07d723dccc082fa1b5bbf892ccf1f9
skris92 <szurcsik92@gmail.com>
2022-08-23 15:08:50 +0200

    UserCreateDTO

---
+++ SkrisForum.Core/Model/UserDTOs/UserCreateDTO.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SkrisForum.Core.Model.UserDTOs
+{
+    public class UserCreateDTO
+    {
+        [Required]
+        [MaxLength(20), MinLength(2)]
+        public string Username { get; set; }
+
+        [Required]
+        [EmailAddress]
+        [MaxLength(50)]
+        public string EmailAddress { get; set; }
+
+        [Required]
+        [MaxLength(20), MinLength(4)]
+        public string Password { get; set; }
+
+        public string? Role { get; set; }
+    }
+}
