23c292df6b0f969904f4ee5bb4c959037879fd44
skris92 <szurcsik92@gmail.com>
2022-08-24 12:35:50 +0200

    UserUpdateDTO

---
+++ SkrisForum.Core/Model/UserDTOs/UserUpdateDTO.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SkrisForum.Core.Model.UserDTOs
+{
+    public class UserUpdateDTO
+    {
+        public Guid? Id { get; set; }
+
+        public string? EmailAddress { get; set; }
+
+        public string? Username { get; set; }
+
+        public string? Password { get; set; }
+
+        public string? Role { get; set; }
+    }
+}
