3c315f696c7a54174811eb0215983a1b4ab10300
skris92 <szurcsik92@gmail.com>
2022-08-30 13:35:50 +0200

    IUserService interface

---
+++ SkrisForum/Services/IUserService.cs
@@ -0,0 +1,14 @@
+﻿using SkrisForum.Core.Model.UserDTOs;
+
+namespace SkrisForum.Services
+{
+    public interface IUserService
+    {
+        Task<List<UserViewDTO>> GetAllUsers();
+        Task<UserViewDTO> GetUserById(Guid userId);
+        Task<UserLoginDTO> GetLoginDTOByUsername(string username);
+        Task<UserViewDTO> AddUser(UserCreateDTO newUser);
+        Task<UserViewDTO> DeleteUser(Guid userId);
+        Task<UserViewDTO> UpdateUser(Guid userId, UserUpdateDTO updateDto);
+    }
+}
