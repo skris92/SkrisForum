1f9b85e85d230d737739557fc107d5f97d01f099
skris92 <szurcsik92@gmail.com>
2022-08-30 12:44:17 +0200

    Basic routing

--- SkrisForum/Client/skris-forum/src/App.js
+++ SkrisForum/Client/skris-forum/src/App.js
@@ -1,24 +1,21 @@
-import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Layout from './components/Layout';
+import LandingPage from './components/LandingPage';
+import Home from './components/Home';
+import NotPage from './components/NotPage';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<LandingPage />} />
+          <Route path="home" element={<Home />} />
+          <Route path="*" element={<NotPage />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
