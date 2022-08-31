3d4f13118b00204362d50429a678606f369094ba
skris92 <szurcsik92@gmail.com>
2022-08-31 19:20:20 +0200

    Form now registers users in database, flash error messages, navigating after successful registration

--- SkrisForum/Client/skris-forum/src/components/Register.js
+++ SkrisForum/Client/skris-forum/src/components/Register.js
@@ -3,15 +3,22 @@ import Button from 'react-bootstrap/Button';
 import Card from 'react-bootstrap/Card';
 import { LinkContainer } from 'react-router-bootstrap';
 import { useState, useEffect, useRef } from 'react';
+import axios from 'axios';
+import { useNavigate } from 'react-router-dom';
 
 export default Register;
 
+const REGISTER_API_URL = "https://localhost:7171/api/users";
 
 function Register() {
+    const navigate = useNavigate();
 
+    const email = useRef();
+    const username = useRef();
     const password = useRef();
     const cPassword = useRef();
     const [showErrorMessage, setShowErrorMessage] = useState(false);
+    const [errorMessage, setErrorMessage] = useState(null);
     const [passwordClass, setPasswordClass] = useState('form-control');
     const [cPasswordClass, setCPasswordClass] = useState('form-control');
     const [isPasswordValid, setIsPasswordValid] = useState(false);
@@ -21,12 +28,16 @@ function Register() {
         if (password.current.value.length >= 4 && password.current.value.length <= 20) {
             setIsPasswordValid(true);
             setPasswordClass("form-control is-valid");
+            setShowErrorMessage(false);
         } else if (password.current.value === "") {
             setIsPasswordValid(false);
             setPasswordClass("form-control");
+            setShowErrorMessage(false);
         } else {
             setIsPasswordValid(false);
             setPasswordClass("form-control is-invalid");
+            setErrorMessage("At least 4 characters!");
+            setShowErrorMessage(true);
         }
         if (cPassword.current.value !== "") {
             checkPasswords();
@@ -36,16 +47,37 @@ function Register() {
     const checkPasswords = (e) => {
         if (password.current.value === cPassword.current.value && password.current.value.length >= 4 && password.current.value.length <= 20) {
             setIsCPasswordValid(true);
-            setShowErrorMessage(false);
             setCPasswordClass('form-control is-valid');
+            setShowErrorMessage(false);
         } else if (cPassword.current.value === "") {
             setIsCPasswordValid(false);
-            setShowErrorMessage(false);
             setCPasswordClass('form-control');
+            setShowErrorMessage(false);
         } else {
             setIsCPasswordValid(false);
-            setShowErrorMessage(true);
             setCPasswordClass('form-control is-invalid');
+            if (password.current.value.length < 4) {
+                setErrorMessage("At least 4 characters!");
+            } else {
+                setErrorMessage("Passwords doesn't match!");
+            }
+            setShowErrorMessage(true);
+        }
+    }
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+
+        try {
+            await axios.post(REGISTER_API_URL, {
+                "emailAddress": email.current.value,
+                "username": username.current.value,
+                "password": password.current.value
+            });
+            navigate("/login");
+        } catch (error) {
+            setErrorMessage(error.message + "!");
+            setShowErrorMessage(true);
         }
     }
 
@@ -55,7 +87,7 @@ function Register() {
                 <Card.Header><h2>Registration</h2></Card.Header>
                 <Card.Body>
                     <Card.Text>
-                        <Form>
+                        <Form onSubmit={handleSubmit}>
                             <Form.Group>
                                 <Form.Control
                                     id="email"
@@ -63,6 +95,7 @@ function Register() {
                                     maxLength="50"
                                     placeholder="Email Address"
                                     required
+                                    ref={email}
                                 />
                             </Form.Group>
                             <br />
@@ -74,6 +107,7 @@ function Register() {
                                     maxLength="20"
                                     placeholder="Username"
                                     required
+                                    ref={username}
                                 />
                             </Form.Group>
                             <br />
@@ -104,6 +138,7 @@ function Register() {
                             </Form.Group>
                             <br />
                             <Button disabled={!isPasswordValid || (isPasswordValid && !isCPasswordValid)} variant="dark" type="submit">Register</Button>
+                            {showErrorMessage && <span style={{float: "right", color: "red"}}>{errorMessage}</span>}
                         </Form>
                     </Card.Text>
                 </Card.Body>
