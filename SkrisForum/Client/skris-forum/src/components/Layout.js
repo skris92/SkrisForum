71ea0db42490ae5ab585d966fd8f375d97969312
skris92 <szurcsik92@gmail.com>
2022-08-31 10:30:22 +0200

    Basic navigation bar

--- SkrisForum/Client/skris-forum/src/components/Layout.js
+++ SkrisForum/Client/skris-forum/src/components/Layout.js
@@ -1,21 +1,15 @@
 import { Outlet, Link } from "react-router-dom";
+import NavBar from "./NavBar";
 
 export default Layout;
 
 function Layout() {
     return (
-        <nav className="nav-bar">
-            <ul style={{listStyleType: "none"}}>
-                <li>
-                    <Link to="/"><h1>Landing Page</h1></Link>
-                </li>
-                <li>
-                    <Link to="/home">Home</Link>
-                </li>
-            </ul>
+        <div>
+            <NavBar />
             <div className="page-content">
                 <Outlet />
             </div>
-        </nav>
+        </div>
     )
 }
---
+++ SkrisForum/Client/skris-forum/src/components/NavBar.js
@@ -0,0 +1,28 @@
+import { Container, Nav, Navbar } from "react-bootstrap";
+import { LinkContainer } from "react-router-bootstrap";
+
+export default NavBar;
+
+function NavBar() {
+    return (
+        <Navbar bg="dark" variant="dark">
+            <Container>
+                <LinkContainer to="/">
+                    <Navbar.Brand>SkrisForum</Navbar.Brand>
+                </LinkContainer>
+
+                <Nav className="me-auto">
+                    <LinkContainer to="/home">
+                        <Nav.Link>Home</Nav.Link>
+                    </LinkContainer>
+                </Nav>
+
+                <Nav>
+                    <LinkContainer>
+                        <Nav.Link>Login</Nav.Link>
+                    </LinkContainer>
+                </Nav>
+            </Container>
+        </Navbar>
+    )
+}
