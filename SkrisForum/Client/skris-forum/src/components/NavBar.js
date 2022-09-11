00aed4b7c11f5f94a979396cd4251d73f1cf259d
skris92 <szurcsik92@gmail.com>
2022-09-11 15:45:05 +0200

    Navbar collapse

--- SkrisForum/Client/skris-forum/src/components/NavBar.js
+++ SkrisForum/Client/skris-forum/src/components/NavBar.js
@@ -5,23 +5,29 @@ export default NavBar;
 
 function NavBar() {
     return (
-        <Navbar bg="dark" variant="dark">
+        <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
             <Container>
                 <LinkContainer to="/">
                     <Navbar.Brand>SkrisForum</Navbar.Brand>
                 </LinkContainer>
 
-                <Nav className="me-auto">
-                    <LinkContainer to="/home">
-                        <Nav.Link>Home</Nav.Link>
-                    </LinkContainer>
-                </Nav>
+                <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+                <Navbar.Collapse id="responsive-navbar-nav">
 
-                <Nav>
-                    <LinkContainer to="/login">
-                        <Nav.Link>Sign In</Nav.Link>
-                    </LinkContainer>
-                </Nav>
+                    <Nav className="me-auto">
+                        <LinkContainer to="/home">
+                            <Nav.Link>Home</Nav.Link>
+                        </LinkContainer>
+                    </Nav>
+
+
+                    <Nav>
+                        <LinkContainer to="/login">
+                            <Nav.Link>Sign In</Nav.Link>
+                        </LinkContainer>
+                    </Nav>
+                </Navbar.Collapse>
+
             </Container>
         </Navbar>
     )
