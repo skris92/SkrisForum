b3fdec97ddb7af6e8e26a96bf70f1c2c3f0f0af2
skris92 <szurcsik92@gmail.com>
2022-09-11 16:07:47 +0200

    Placeholder content

--- SkrisForum/Client/skris-forum/src/components/Home.js
+++ SkrisForum/Client/skris-forum/src/components/Home.js
@@ -1,20 +1,6 @@
-import { useEffect } from "react";
-import { useNavigate } from "react-router-dom";
-
 export default Home;
 
 function Home() {
-    const navigate = useNavigate();
-
-    useEffect(() => {
-        const date = Date.now();
-        const expMins = JSON.parse(localStorage.getItem("auth"))?.exp * 1000;
-        if (!localStorage.getItem("auth") || date > expMins) {
-            localStorage.removeItem("auth");
-            navigate("/");
-        }
-    }, []);
-
     return (
         <h2>Home content</h2>
     )
