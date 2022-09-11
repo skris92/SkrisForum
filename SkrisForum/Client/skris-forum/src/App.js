158a0b50a6adea6fa2eebcb6281d9c0c3a1199ef
skris92 <szurcsik92@gmail.com>
2022-09-11 23:43:47 +0200

    BrowserRouter moved to index.js to surround AuthProvider

--- SkrisForum/Client/skris-forum/src/App.js
+++ SkrisForum/Client/skris-forum/src/App.js
@@ -1,5 +1,6 @@
 import './App.css';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
+import RequireAuth from './components/RequireAuth';
 import Layout from './components/Layout';
 import LandingPage from './components/LandingPage';
 import Home from './components/Home';
@@ -9,17 +10,15 @@ import Register from './components/Register';
 
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Layout />}>
-          <Route index element={<LandingPage />} />
-          <Route path="login" element={<Login />} />
-          <Route path="register" element={<Register />} />
-          <Route path="home" element={<Home />} />
-          <Route path="*" element={<NotPage />} />
-        </Route>
-      </Routes>
-    </BrowserRouter>
+    <Routes>
+      <Route path="/" element={<Layout />}>
+        <Route index element={<LandingPage />} />
+        <Route path="login" element={<Login />} />
+        <Route path="register" element={<Register />} />
+        <Route path="home" element={<RequireAuth allowedRoles={["ADMIN", "USER"]}><Home /></RequireAuth>} />
+        <Route path="*" element={<NotPage />} />
+      </Route>
+    </Routes>
   );
 }
 
