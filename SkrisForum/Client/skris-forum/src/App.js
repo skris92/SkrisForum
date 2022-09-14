1290a8d88e28b636909ad0800d1b928f5d59dc12
skris92 <szurcsik92@gmail.com>
2022-09-14 14:33:17 +0200

    /profile protected route

--- SkrisForum/Client/skris-forum/src/App.js
+++ SkrisForum/Client/skris-forum/src/App.js
@@ -1,14 +1,15 @@
 import './App.css';
 import { Routes, Route } from 'react-router-dom';
+import { useEffect } from 'react';
 import RequireAuth from './components/RequireAuth';
+import useAuth from './hooks/useAuth';
 import Layout from './components/Layout';
 import LandingPage from './components/LandingPage';
 import Browse from './components/Browse';
 import NotPage from './components/NotPage';
 import Login from './components/Login';
 import Register from './components/Register';
-import useAuth from './hooks/useAuth';
-import { useEffect } from 'react';
+import Profile from './components/Profile';
 
 function App() {
   const { auth, logout, checkExpired } = useAuth();
@@ -26,6 +27,7 @@ function App() {
         <Route path="login" element={<Login />} />
         <Route path="register" element={<Register />} />
         <Route path="browse" element={<RequireAuth allowedRoles={["ADMIN", "USER"]}><Browse /></RequireAuth>} />
+        <Route path="profile" element={<RequireAuth allowedRoles={["ADMIN", "USER"]}><Profile /></RequireAuth>} />
         <Route path="*" element={<NotPage />} />
       </Route>
     </Routes>
