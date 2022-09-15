5d12bc7f8581389944cd9732e0626db3a2c4c7ff
skris92 <szurcsik92@gmail.com>
2022-09-15 13:27:20 +0200

    StrictMode turned off

--- SkrisForum/Client/skris-forum/src/index.js
+++ SkrisForum/Client/skris-forum/src/index.js
@@ -8,13 +8,13 @@ import AuthProvider from './context/AuthProvider';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
+  // <React.StrictMode>
     <BrowserRouter>
       <AuthProvider>
         <App />
       </AuthProvider>
     </BrowserRouter>
-  </React.StrictMode>
+  // </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
